8036f29682bcdb8ab36a6e1e6a2ae3518286b6d2
ZwitterKaneda <smentek@gmail.com>
2023-09-21 11:55:51 -0300

    save changes

--- tests/coverage/test_swaglabs_dani.py
+++ tests/coverage/test_swaglabs_dani.py
@@ -2,9 +2,12 @@ import pytest
 from tests.testbase import *
 
 class TestSuiteLogin:
+    #*TC1
 	def test_login_success(self, beforeEach: Test):
 		web, get = beforeEach
-  
+		get.byDataTest('username').send_keys('standard_user')
+		get.byDataTest('password').send_keys('secret_sauce')
+		get.byDataTest('login-button').click()
 
 if __name__ == "__main__":
     pytest.main()
