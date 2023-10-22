fbd93ed7003532c8c5b6f0bfba9a014a80617578
PabloCz <Pablotest@gmail.com>
2023-10-22 16:36:23 -0300

    GX-40707 Add LoginPage , precondition lista

--- tests/coverage/PLP/test_GX-40707_filterProducts.py
+++ tests/coverage/PLP/test_GX-40707_filterProducts.py
@@ -1,1 +1,28 @@
+import pytest
 from tests.testbase import *
+from tests.pages.GX_40707_LoginPage import LoginPage
+
+
+def initialSetup():
+    global get, web, loginPage
+    web = Drivers().chromeDriver()
+    get = Locators(web)
+    loginPage = LoginPage(web, get)
+
+
+def loginPrecondition():
+    get.page('https://www.saucedemo.com/')
+    loginPage.login(username='standard_user', password='secret_sauce')
+    loginPage.clickButtonSubmit()
+    Url = web.current_url
+    assert '/inventory' in Url
+
+
+@pytest.fixture
+def beforeEach():
+    initialSetup()
+    loginPrecondition()
+
+
+def test_TC01(beforeEach):
+    pass
